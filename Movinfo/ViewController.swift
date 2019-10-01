8dd4d831b3c64ea3a5c1871b07928e422483012e
Aydin <aydin@katu.com.tr>
2019-10-01 19:59:28 +0300

    Initial Commit

---
+++ Movinfo/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Movinfo
+//
+//  Created by Aydn on 1.10.2019.
+//  Copyright © 2019 aydinsarican. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
